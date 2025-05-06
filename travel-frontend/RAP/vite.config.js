118b0415d8eafd708ff6301c322eaf9c374b8df3
KARLE-ATHARVA <atharvakarle04@gmail.com>
2025-05-06 23:06:19 +0530

    PUSH

--- travel-frontend/RAP/vite.config.js
+++ travel-frontend/RAP/vite.config.js
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'./',
+  base: './',
   plugins: [react()],
   server: {
     proxy: {
@@ -11,11 +11,12 @@ export default defineConfig({
         target: 'https://k5m74a3y7k.execute-api.eu-north-1.amazonaws.com',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '/prod'),
-        secure: false,
-        headers: {
-          // Add any required headers here
-        }
+        secure: false
       }
     }
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
   }
-})
+});
